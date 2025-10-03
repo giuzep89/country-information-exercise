486837324f910572c0e64e4dcf58611b4a6fdb4b
giuzep89 <g.rinaldimusic@gmail.com>
2025-10-03 18:45:55 +0200

    Function written

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css';
+import axios from "axios";
 
 function App() {
+    async function fetchAllCountries() {
+        const result = await
+            axios.get("https://restcountries.com/v3.1/all?fields=name,flag,population");
+            console.log(result);
+    }
+
+
+
 
     return (
         <>
-            <header></header>
-            <div className="country-search"></div>
+            <header>
+                <h1>World Regions</h1>
+            </header>
+            <div className="country-info">
+                <button type="button" onClick={fetchAllCountries}>Show info on all countries!</button>
+            </div>
         </>
     )
 }
