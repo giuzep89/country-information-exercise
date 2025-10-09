943ab15588c39b6196de466d26e766fb0e5d5b80
giuzep89 <g.rinaldimusic@gmail.com>
2025-10-09 13:44:33 +0200

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,42 @@ import './App.css';
 import axios from "axios";
 import {useState} from "react";
 import setRegionColor from "./helpers/returnRegionColor.jsx";
+import globePicture from "./assets/images/Globe_clip_art.svg";
 
 function App() {
-    const [allCountries, setAllCountries] = useState({});
-    const [clicked, setClicked] = useState(false);
+    const [allCountries, setAllCountries] = useState([]);
+    const [worldCountriesButton, setWorldCountriesButton] = useState(false);
+    const [country, setCountry] = useState({});
+    const [singleCountryButton, setSingleCountryButton] = useState(false);
 
     async function fetchAllCountries() {
         const result = await
             axios.get("https://restcountries.com/v3.1/all?fields=name,flag,population,region,cca3");
-        setAllCountries(result);
-        setClicked(true);
+        setAllCountries(result.data);
+    }
+
+    async function fetchCountryByName() {
+        const result = await
+            axios.get("https://restcountries.com/v3.1/name/netherlands?fullText=true");
+        setCountry(result.data[0]);
+        console.log(result.data);
+    }
+
+    function handleClick(type){
+        if (type === "allCountries") {
+            fetchAllCountries();
+            setWorldCountriesButton(true);
+        } else if (type === "country") {
+            fetchCountryByName();
+            setSingleCountryButton(true);
+        }
     }
 
     // <<<<<---------------------------## TESTING ZONE ##-------------------------->>>>>>
 
-    console.log(clicked);
-    console.log(allCountries);
+    // console.log(clicked);
+    // console.log(allCountries);
+    // console.log(country);
 
     // <<<<<----------------------------------------------------------------------->>>>>>
 
@@ -26,19 +46,38 @@ function App() {
             <header>
                 <h1>World Regions</h1>
             </header>
-            <div className="countries-section">
 
-                {!(clicked) && <button type="button" onClick={fetchAllCountries}>Show info on all countries!</button>}
+            <section className="countries-section">
 
-                {(clicked) &&
-                    <ul className="countries-container">{allCountries.data.map((country) => {
+                {!(worldCountriesButton) && <button type="button" onClick={() => handleClick("allCountries")}>Show info on all countries!</button>}
+
+                {worldCountriesButton &&
+                    <ul className="countries-container">{allCountries.map((country) => {
                         // eslint-disable-next-line react/jsx-key
                         return <li key={country.cca3} className="country-card">{country.flag} <strong
                             className={setRegionColor(country.region)}>{country.name.common}</strong> has a population
                             of {country.population} people</li>
                     })}</ul>
                 }
-            </div>
+            </section>
+
+            <section className="search-country-section">
+                <div className="search-country-container">
+                    <h2>Search country information</h2>
+                    <div className="globe-image"><img src={globePicture} alt="globe image"/></div>
+
+                    {!(singleCountryButton) && <button type="button" onClick={() => handleClick("country")}>Zoek</button>}
+
+                    {/* I added country.name under here as an extra condition because otherwise React would try to render the block of code with country.name still --undefined-- and break the page, so I had to add it to make sure it had data in it before the rendering. I don't quite understand it but it works as it is.*/}
+
+                    {singleCountryButton && country.name &&
+                        <div className="country-info">
+                        <h2>{country.flag} {country.name.common}</h2>
+                        <p>{country.name.common} is situated in {country.subregion} and the capital is {country.capital[0]}.</p>
+                            <p>It has {country.population} people</p>
+                    </div>}
+                </div>
+            </section>
         </>
     )
 }
---
+++ src/helpers/convertAndRoundUp.jsx
@@ -0,0 +1,4 @@
+export default function convertAndRoundUp(number) {
+
+    return Math.round(number);
+}
